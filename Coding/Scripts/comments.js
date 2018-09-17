bbd7c604dbaa2a0270e4dee0fe35adb0bd5226ad
stephenbarrett <mail@steviebarrett.com>
2018-09-17 13:14:33 +0100

    Changed AJAX URLs

--- Coding/Scripts/comments.js
+++ Coding/Scripts/comments.js
@@ -7,7 +7,7 @@ $(function() {
     $('.addComment').on('click', function() {
         var sid = $(this).attr('data-n');
         var comment = 'This is a test comment';
-        $.getJSON('https://dasg.ac.uk/ajax/manuscripts.php?action=saveComment&sid='+sid+'&comment='+comment, function(data) {
+        $.getJSON('/ajax/manuscripts.php?action=saveComment&sid='+sid+'&comment='+comment, function(data) {
             console.log(data)
         })
     })
@@ -17,7 +17,7 @@ $(function() {
      */
     $('.viewComment').on('click', function() {
         var sid = $(this).attr('data-n');
-        $.getJSON('https://dasg.uk/ajax/manuscripts.php?action=getComment&sid='+sid, function(data) {
+        $.getJSON('/ajax/manuscripts.php?action=getComment&sid='+sid, function(data) {
             console.log(data)
         })
     })
