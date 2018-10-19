00a02f4d7b77eb684b7aa798c3ff35ae17930cad
steviebarrett <mail@steviebarrett.com>
2018-10-19 09:28:27 +0100

    callback handler added to deleteComment AJAX call

--- Coding/Scripts/comments.js
+++ Coding/Scripts/comments.js
@@ -56,20 +56,26 @@ $(function() {
         $(this).parent().addClass('greyedOut');   //fade the comment
         var parts = $(this).attr('id').split('__');
         var cid = parts[1];     //the DB comment ID
-        $.getJSON('/ajax/manuscripts.php?action=deleteComment&cid='+cid, function(data) {
-            console.log(data);
-            if (data.deleted == true) {
-                feedbackHtml = '<strong>The comment has been deleted</strong>';
-                // if (data.empty == true) { //if the number of deleted comments = the total number of comments
-                //     $.getJSON('/ajax/manuscripts.php?action=getCommentInfo&cid='+cid, function (cdata) {
-                //         $('a[data-s='+cdata.section+'][data-n='+cdata.section_id+']').addClass('greyedOut');  //fade the viewcComment link
-                //     })
-                // }
-                console.log(feedbackHtml);
-                console.log(data.empty);
-            } else { //there was an error deleting the comment
+        var feedbackHtml;
+        $.getJSON('/ajax/manuscripts.php?action=deleteComment&cid='+cid, function() {
+        })
+            .done(function(data) {
+                console.log(data);
+                if (data.deleted == true) {
+                    feedbackHtml = '<strong>The comment has been deleted</strong>';
+                    // if (data.empty == true) { //if the number of deleted comments = the total number of comments
+                    //     $.getJSON('/ajax/manuscripts.php?action=getCommentInfo&cid='+cid, function (cdata) {
+                    //         $('a[data-s='+cdata.section+'][data-n='+cdata.section_id+']').addClass('greyedOut');  //fade the viewcComment link
+                    //     })
+                    // }
+                    console.log(feedbackHtml);
+                    console.log(data.empty);
+                }
+            })
+            .fail(function() {
                 feedbackHtml = '<em>There was an error deleting the comment</em>';
-            }
+            });
+            console.log(feedbackHtml);
         });
     });
 
