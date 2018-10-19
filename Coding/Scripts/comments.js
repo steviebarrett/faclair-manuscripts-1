64791b57f077cdc895d12a34a54af512f7c33f86
steviebarrett <mail@steviebarrett.com>
2018-10-19 09:14:33 +0100

    Realtime update of greyed out viewComment link on fully deleted section

--- Coding/Scripts/comments.js
+++ Coding/Scripts/comments.js
@@ -60,12 +60,18 @@ $(function() {
             console.log(data);
             if (data.deleted == true) {
                 feedbackHtml = '<strong>The comment has been deleted</strong>';
+                if (data.sec == data.del) { //if the number of deleted comments = the total number of comments
+                    $.getJSON('/ajax/manuscripts.php?action=getCommentInfo&cid='+cid, function (cdata) {
+                        $('a[data-s='+cdata.section+'][data-n='+cdata.section_id+']').addClass('greyedOut');  //fade the viewcComment link
+                    })
+                }
                 console.log(data.empty);
             } else { //there was an error deleting the comment
                 feedbackHtml = '<em>There was an error deleting the comment</em>';
             }
         });
     });
+
     /*
         Get comments
      */
