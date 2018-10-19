39e3144e083e14606629c75f98a1eb154127ee05
steviebarrett <mail@steviebarrett.com>
2018-10-19 09:37:53 +0100

    Bug fix

--- Coding/Scripts/comments.js
+++ Coding/Scripts/comments.js
@@ -62,19 +62,16 @@ $(function() {
                 console.log(data);
                 if (data.deleted == true) {
                     feedbackHtml = '<strong>The comment has been deleted</strong>';
-                    // if (data.empty == true) { //if the number of deleted comments = the total number of comments
-                    //     $.getJSON('/ajax/manuscripts.php?action=getCommentInfo&cid='+cid, function (cdata) {
-                    //         $('a[data-s='+cdata.section+'][data-n='+cdata.section_id+']').addClass('greyedOut');  //fade the viewcComment link
-                    //     })
-                    // }
-                   // console.log(feedbackHtml);
-                   // console.log(data.empty);
+                    if (data.empty == true) { //if the number of deleted comments = the total number of comments
+                        $.getJSON('/ajax/manuscripts.php?action=getCommentInfo&cid='+cid, function (cdata) {
+                            $('a[data-s='+cdata.section+'][data-n='+cdata.section_id+']').addClass('greyedOut');  //fade the viewcComment link
+                        })
+                    }
                 }
             })
             .fail(function() {
                 feedbackHtml = '<em>There was an error deleting the comment</em>';
             });
-            console.log(feedbackHtml);
     });
 
     /*
