4271adc4dd3f4c29e3b6ce72eb98155753d52ac1
conmaol <Mark.McConville@glasgow.ac.uk>
2019-09-15 19:56:41 +0100

    index file

---
+++ index.php
@@ -0,0 +1,23 @@
+<html lang="en">
+  <head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"/>
+    <!-- <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"/>
+    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/bPopup/0.11.0/jquery.bpopup.min.js"/>-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>    
+    <title>MSS</title>
+  </head>
+  <body>
+    <div class="container-fluid">
+      <div class="list-group list-group-flush">
+        <div class="list-group-item list-group-item-action">Transcription 1: <a href="Coding/Scripts/viewDiplomatic.php?t=1">diplomatic</a>, <a href="Coding/Scripts/viewSemiDiplomatic.php?t=1">semi-diplomatic</a></div>
+        <div class="list-group-item list-group-item-action">Transcription 6: <a href="Coding/Scripts/viewDiplomatic.php?t=6">diplomatic</a>, <a href="Coding/Scripts/viewSemiDiplomatic.php?t=6">semi-diplomatic</a></div>
+        <div class="list-group-item list-group-item-action">Transcription 17: <a href="Coding/Scripts/viewDiplomatic.php?t=17">diplomatic</a>, <a href="Coding/Scripts/viewSemiDiplomatic.php?t=17">semi-diplomatic</a></div>
+      </div>
+    </div>    
+  </body>
+  <!--  -->
+</html>
