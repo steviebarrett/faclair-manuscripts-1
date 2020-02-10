71140ff923656e694d01968a343facb1ec69a22b
conmaol <Mark.McConville@glasgow.ac.uk>
2020-02-10 17:53:46 +0000

    added new MSS to index

--- index.php
+++ index.php
@@ -14,7 +14,6 @@
     <div class="container-fluid">
       <nav class="navbar navbar-dark bg-primary fixed-top navbar-expand-lg">
         <a class="navbar-brand" href="about.php">DASG-MSS</a>
-        
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -26,9 +25,35 @@
       </nav>
       <div class="list-group list-group-flush">
         <div class="list-group-item list-group-item-action">Transcription 1 (<em>The Book of Deer</em>: CUL I.i.6.32): <a href="Coding/Scripts/viewTranscription.php?t=1&diplo=yes">diplo</a>, <a href="Coding/Scripts/viewTranscription.php?t=1&diplo=no">semi</a></div>
+        <div class="list-group-item list-group-item-action">Transcription 2 (): <a href="Coding/Scripts/viewTranscription.php?t=2&diplo=yes">diplo</a>, <a href="Coding/Scripts/viewTranscription.php?t=2&diplo=no">semi</a></div>
         <div class="list-group-item list-group-item-action">Transcription 3 (<em>Cath Fionntrágha</em>: NLS Adv. MS. 72.2.11): <a href="Coding/Scripts/viewTranscription.php?t=3&diplo=yes">diplo</a>, <a href="Coding/Scripts/viewTranscription.php?t=3&diplo=no">semi</a></div>
+        <div class="list-group-item list-group-item-action">Transcription 4 (): <a href="Coding/Scripts/viewTranscription.php?t=4&diplo=yes">diplo</a>, <a href="Coding/Scripts/viewTranscription.php?t=4&diplo=no">semi</a></div>
+        <div class="list-group-item list-group-item-action">Transcription 5 (): <a href="Coding/Scripts/viewTranscription.php?t=5&diplo=yes">diplo</a>, <a href="Coding/Scripts/viewTranscription.php?t=5&diplo=no">semi</a></div>
         <div class="list-group-item list-group-item-action">Transcription 6 (NLS Adv. 72.1.31): <a href="Coding/Scripts/viewTranscription.php?t=6&diplo=yes">diplo</a>, <a href="Coding/Scripts/viewTranscription.php?t=6&diplo=no">semi</a></div>
+        <div class="list-group-item list-group-item-action">Transcription 7 (): <a href="Coding/Scripts/viewTranscription.php?t=7&diplo=yes">diplo</a>, <a href="Coding/Scripts/viewTranscription.php?t=7&diplo=no">semi</a></div>
+        <div class="list-group-item list-group-item-action">Transcription 8 (): <a href="Coding/Scripts/viewTranscription.php?t=8&diplo=yes">diplo</a>, <a href="Coding/Scripts/viewTranscription.php?t=8&diplo=no">semi</a></div>
+        <div class="list-group-item list-group-item-action">Transcription 9 (): <a href="Coding/Scripts/viewTranscription.php?t=9&diplo=yes">diplo</a>, <a href="Coding/Scripts/viewTranscription.php?t=9&diplo=no">semi</a></div>
+        <div class="list-group-item list-group-item-action">Transcription 10 (): <a href="Coding/Scripts/viewTranscription.php?t=10&diplo=yes">diplo</a>, <a href="Coding/Scripts/viewTranscription.php?t=10&diplo=no">semi</a></div>
+        <div class="list-group-item list-group-item-action">Transcription 11 (): <a href="Coding/Scripts/viewTranscription.php?t=11&diplo=yes">diplo</a>, <a href="Coding/Scripts/viewTranscription.php?t=11&diplo=no">semi</a></div>
+        <div class="list-group-item list-group-item-action">Transcription 12 (): <a href="Coding/Scripts/viewTranscription.php?t=12&diplo=yes">diplo</a>, <a href="Coding/Scripts/viewTranscription.php?t=12&diplo=no">semi</a></div>
+        <div class="list-group-item list-group-item-action">Transcription 13 (): <a href="Coding/Scripts/viewTranscription.php?t=13&diplo=yes">diplo</a>, <a href="Coding/Scripts/viewTranscription.php?t=13&diplo=no">semi</a></div>
+        <div class="list-group-item list-group-item-action">Transcription 14 (): <a href="Coding/Scripts/viewTranscription.php?t=14&diplo=yes">diplo</a>, <a href="Coding/Scripts/viewTranscription.php?t=14&diplo=no">semi</a></div>
+        <div class="list-group-item list-group-item-action">Transcription 15 (): <a href="Coding/Scripts/viewTranscription.php?t=15&diplo=yes">diplo</a>, <a href="Coding/Scripts/viewTranscription.php?t=15&diplo=no">semi</a></div>
+        <div class="list-group-item list-group-item-action">Transcription 16 (): <a href="Coding/Scripts/viewTranscription.php?t=16&diplo=yes">diplo</a>, <a href="Coding/Scripts/viewTranscription.php?t=16&diplo=no">semi</a></div>
         <div class="list-group-item list-group-item-action">Transcription 17 (NLS Adv. 72.1.4): <a href="Coding/Scripts/viewTranscription.php?t=17&diplo=yes">diplo</a>, <a href="Coding/Scripts/viewTranscription.php?t=17&diplo=no">semi</a></div>
+        <div class="list-group-item list-group-item-action">Transcription 18 (): <a href="Coding/Scripts/viewTranscription.php?t=18&diplo=yes">diplo</a>, <a href="Coding/Scripts/viewTranscription.php?t=18&diplo=no">semi</a></div>
+        <div class="list-group-item list-group-item-action">Transcription 19 (<em>Mór an Broinsgel</em>): <a href="Coding/Scripts/viewTranscription.php?t=19&diplo=yes">diplo</a>, <a href="Coding/Scripts/viewTranscription.php?t=19&diplo=no">semi</a></div>
+        <div class="list-group-item list-group-item-action">Transcription 20 (): <a href="Coding/Scripts/viewTranscription.php?t=20&diplo=yes">diplo</a>, <a href="Coding/Scripts/viewTranscription.php?t=20&diplo=no">semi</a></div>
+        <div class="list-group-item list-group-item-action">Transcription 21 (): <a href="Coding/Scripts/viewTranscription.php?t=21&diplo=yes">diplo</a>, <a href="Coding/Scripts/viewTranscription.php?t=21&diplo=no">semi</a></div>
+        <div class="list-group-item list-group-item-action">Transcription 22 (): <a href="Coding/Scripts/viewTranscription.php?t=22&diplo=yes">diplo</a>, <a href="Coding/Scripts/viewTranscription.php?t=22&diplo=no">semi</a></div>
+        <div class="list-group-item list-group-item-action">Transcription 23 (): <a href="Coding/Scripts/viewTranscription.php?t=23&diplo=yes">diplo</a>, <a href="Coding/Scripts/viewTranscription.php?t=23&diplo=no">semi</a></div>
+        <div class="list-group-item list-group-item-action">Transcription 24 (): <a href="Coding/Scripts/viewTranscription.php?t=24&diplo=yes">diplo</a>, <a href="Coding/Scripts/viewTranscription.php?t=24&diplo=no">semi</a></div>
+        <div class="list-group-item list-group-item-action">Transcription 25 (): <a href="Coding/Scripts/viewTranscription.php?t=25&diplo=yes">diplo</a>, <a href="Coding/Scripts/viewTranscription.php?t=25&diplo=no">semi</a></div>
+        <div class="list-group-item list-group-item-action">Transcription 26 (NLS Adv. 72.1.34): <a href="Coding/Scripts/viewTranscription.php?t=26&diplo=yes">diplo</a>, <a href="Coding/Scripts/viewTranscription.php?t=26&diplo=no">semi</a></div>
+        <div class="list-group-item list-group-item-action">Transcription 27 (): <a href="Coding/Scripts/viewTranscription.php?t=27&diplo=yes">diplo</a>, <a href="Coding/Scripts/viewTranscription.php?t=27&diplo=no">semi</a></div>
+        <div class="list-group-item list-group-item-action">Transcription 28 (): <a href="Coding/Scripts/viewTranscription.php?t=28&diplo=yes">diplo</a>, <a href="Coding/Scripts/viewTranscription.php?t=28&diplo=no">semi</a></div>
+        <div class="list-group-item list-group-item-action">Transcription 29 (): <a href="Coding/Scripts/viewTranscription.php?t=29&diplo=yes">diplo</a>, <a href="Coding/Scripts/viewTranscription.php?t=29&diplo=no">semi</a></div>
+        <div class="list-group-item list-group-item-action">Transcription 30 (): <a href="Coding/Scripts/viewTranscription.php?t=30&diplo=yes">diplo</a>, <a href="Coding/Scripts/viewTranscription.php?t=30&diplo=no">semi</a></div>
       </div>
     </div>    
   </body>
